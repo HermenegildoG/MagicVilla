5a8ecf1f83c3ef0ae24cf0e4ec21cf6bf59c3c9c
HermenegildoG <meredossantos@outlook.com>
2023-09-11 23:21:18 -0600

    Db-Context - inyeccion de depedencia

---
+++ MagicVilla_API/Datos/ApplicationDbContext.cs
@@ -0,0 +1,47 @@
+﻿using MagicVilla_API.Modelos;
+using Microsoft.EntityFrameworkCore;
+
+namespace MagicVilla_API.Datos
+{
+    public class ApplicationDbContext:DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext>options):base(options)
+        {
+
+        }
+        public DbSet<Villa> villas { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Villa>().HasData(
+                new Villa()
+                {
+                    Id = 1,
+                    Nombre = "Villa Chica",
+                    Detalle = "Aun sin detalles",
+                    ImagenUrl = "",
+                    Ocupantes = 5,
+                    MetrosCuadrados = 50,
+                    Tarifa = 200,
+                    Amenidad = "",
+                    FechaCreacion = DateTime.Now,
+                    FechaActualizacion = DateTime.Now
+                },
+                new Villa()
+                {
+                    Id = 2,
+                    Nombre = "Amozoc",
+                    Detalle = "Aun sin detalles",
+                    ImagenUrl = "",
+                    Ocupantes = 24,
+                    MetrosCuadrados = 90,
+                    Tarifa = 600,
+                    Amenidad = "",
+                    FechaCreacion = DateTime.Now,
+                    FechaActualizacion = DateTime.Now
+                });
+        }
+
+
+    }
+}
---
+++ MagicVilla_API/Migrations/20230906231257_AlimentarTablaVilla.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace MagicVilla_API.Migrations
+{
+    /// <inheritdoc />
+    public partial class AlimentarTablaVilla : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "villas",
+                columns: new[] { "Id", "Amenidad", "Detalle", "FechaActualizacion", "FechaCreacion", "ImagenUrl", "MetrosCuadrados", "Nombre", "Ocupantes", "Tarifa" },
+                values: new object[,]
+                {
+                    { 1, "", "Aun sin detalles", new DateTime(2023, 9, 6, 17, 12, 56, 868, DateTimeKind.Local).AddTicks(4688), new DateTime(2023, 9, 6, 17, 12, 56, 868, DateTimeKind.Local).AddTicks(4675), "", 50, "Villa Chica", 5, 200.0 },
+                    { 2, "", "Aun sin detalles", new DateTime(2023, 9, 6, 17, 12, 56, 868, DateTimeKind.Local).AddTicks(4760), new DateTime(2023, 9, 6, 17, 12, 56, 868, DateTimeKind.Local).AddTicks(4759), "", 90, "Amozoc", 24, 600.0 }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "villas",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "villas",
+                keyColumn: "Id",
+                keyValue: 2);
+        }
+    }
+}
